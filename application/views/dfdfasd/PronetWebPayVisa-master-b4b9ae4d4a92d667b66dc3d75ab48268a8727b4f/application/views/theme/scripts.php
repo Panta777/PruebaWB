8598c9117c7358def7f3946323a75cf6cbc45dd5
panta Medrano <panta Medrano>
2017-03-14 23:51:29 -0600

    Cambios

--- application/views/dfdfasd/PronetWebPayVisa-master-b4b9ae4d4a92d667b66dc3d75ab48268a8727b4f/application/views/theme/scripts.php
+++
@@ -1,7 +0,0 @@
-﻿<!-- Mainly Scripts -->
-<script type="text/javascript" src="<?php echo base_url("/assets/js/jquery-2.1.1.js"); ?>"></script>
-<script type="text/javascript" src="<?php echo base_url("/assets/js/bootstrap.min.js"); ?>"></script>
-<script type="text/javascript" src="<?php echo base_url("/assets/js/plugins/metisMenu/jquery.metisMenu.js"); ?>"></script>
-<script type="text/javascript" src="<?php echo base_url("/assets/js/plugins/slimscroll/jquery.slimscroll.min.js"); ?>"></script>
-
-
